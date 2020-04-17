f31771e52720a498d8a32cf3f4b80fccf0388d9c
Yna Mataya <ylmataya@yahoo.com>
2020-04-16 19:23:26 -0500

    added badge and github links

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# homework-8
-Readme generator using node
+# Badge:
+(https://img.shields.io/badge/license-APACHE 2.0-blue.svg)]
+
+# Github Project: 
+https://github.com/ymataya/homework-2
+
+# Live Link: 
+https://ymataya.github.io/homework-2/
+
+# Project Title:
+Homework-2
+
+# Project Description:
+asflsdgsg
+
+# Table of Contents:
+* sdgfkjhkasfkd
+
+# Installation:
+sfsgfjerkgrgerh
+
+# Usage:
+gsdgdfgsgdfg
+
+# Credits:
+fdseheth
+
+# License:
+erherherh
+
+# Test:
+erher
+
+# Profile Picture: 
+herheherh
+
+# Github Email: 
+trwefewa
--- index.js
+++ index.js
@@ -11,59 +11,65 @@ const writeFileAsync = util.promisify(fs.writeFile);
 //Questions to ask user to fill out the README file//
 function promptUser() {
     return inquirer.prompt([{
-            type: "input",
+            type: "list",
             name: "badge",
-            message: "Please provide badges."
+            message: "Please provide badges.",
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "none"] 
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What's your Github username:"
         },
         {
             type: "input",
             name: "title",
-            message: "Provide a title for your project."
+            message: "Provide a title for your project:"
         },
         {
             type: "input",
             name: "description",
-            message: "Describe your project."
+            message: "Describe your project:"
         },
         {
             type: "input",
             name: "content",
-            message: "If your README is long, creating a table of contents would be helpful for users."
+            message: "Create a table of contents:"
         },
         {
             type: "input",
             name: "install",
-            message: "Provide a step-by-step description of how to get the development environment running."
+            message: "Provide a step-by-step description of how to get the development environment running:"
         },
         {
             type: "input",
             name: "usage",
-            message: "Provide instructions and examples on how you want users to use your project."
+            message: "Provide instructions and examples on how you want users to use your project:"
         },
         {
             type: "input",
             name: "credit",
-            message: "List anyone who collaborated with you by adding links to their Github profiles. If there were any tutorials or third-party assets that were used, list the creators along with their links."
+            message: "List any help you got to create this project (contributors, third-party, tutorials, etc.): "
         },
         {
             type: "input",
             name: "license",
-            message: "Let other developers know what they can and cannot do with your project."
+            message: "What can and cannot be done to your project:"
         },
         {
             type: "input",
             name: "test",
-            message: "Write tests for your application and provide examples on how to run them."
+            message: "Write tests for your application and provide examples on how to run them:"
         },
         {
             type: "input",
             name: "picture",
-            message: "Provide a file name for your Github profile picture."
+            message: "Paste the URL for your Github profile picture:"
         },
         {
             type: "input",
             name: "email",
-            message: "What is your Github email?"
+            message: "What is your Github email:"
         }
     ]);
 }
@@ -71,38 +77,44 @@ function promptUser() {
 //Generate the README by creating a template and filling it in with user answers//
 function generateReadMe(answer) {
     return `
-    Badge:
-    ${answer.badge}
+# Badge:
+https://img.shields.io/badge/license-${answer.badge}-blue.svg
 
-    Project Title:
-    ${answer.title}
+# Github Project: 
+https://github.com/${answer.username}/${answer.title.toLowerCase().split(" ").join("-")}
 
-    Project Description:
-    ${answer.description}
+# Live Link: 
+https://${answer.username}.github.io/${answer.title.toLowerCase().split(" ").join("-")}/
 
-    Table of Contents:
-    ${answer.content}
+# Project Title:
+${answer.title}
 
-    Installation:
-    ${answer.install}
+# Project Description:
+${answer.description}
 
-    Usage:
-    ${answer.usage}
+# Table of Contents:
+* ${answer.content}
 
-    Credits:
-    ${answer.credit}
+# Installation:
+${answer.install}
 
-    License:
-    ${answer.license}
+# Usage:
+${answer.usage}
 
-    Test:
-    ${answer.test}
+# Credits:
+${answer.credit}
 
-    Profile Picture:
-    ${answer.picture}
+# License:
+${answer.license}
 
-    Github Email:
-    ${answer.email}
+# Test:
+${answer.test}
+
+# Profile Picture: 
+${answer.picture}
+
+# Github Email: 
+${answer.email}
 `;
   }
 
@@ -114,11 +126,11 @@ function generateReadMe(answer) {
     const readMe = generateReadMe(userInput); 
     console.log(readMe);
     // Return or print out a new file called readme.md // Not sure why it's returning this though
-    return writeFileAsync("readme-generator.md", readMe);
+    return writeFileAsync("README.md", readMe);
   })
   .then(function () {
     // If it's successful, it would just say successful //
-    console.log("Successfully wrote to readme-generator.md");
+    console.log("Successfully wrote to README.md");
   })
   .catch(function (err) { // Why catch? //
     // If it's not successful, it would throw an error //
